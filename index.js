9ba2541a46b1585f7d15f28ac61d0db5f058fbae
honpery <honpery@gmail.com>
2017-12-18 17:47:12 +0800

    add submit form get data logic.

--- index.js
+++ index.js
@@ -4,4 +4,23 @@ $(document).ready(function () {
         now.getFullYear() + '-' + now.getMonth()+ '-' + now.getDate() + ' '  +
         now.getHours() + ':' + now.getMinutes() + ':' +now.getSeconds() 
     );
+
+
+    $('#info-form').on('submit', e => {
+        e.preventDefault();
+        
+        var result = $('#info-form').serializeArray().reduce(function(obj, item) {
+            if (item.name === 'ability') {
+                obj[item.name] = obj[item.name] || [];
+                obj[item.name].push(item.value);
+                return obj;
+            }
+            obj[item.name] = item.value;
+            return obj;
+        }, {});
+
+        // send request
+        // todo
+    })
+
 })
