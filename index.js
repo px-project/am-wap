55af7b9bf57466ddc46fdf8c71a34c42817efe36
honpery <honpery@gmail.com>
2017-12-21 18:18:18 +0800

    update footer style.

--- index.js
+++ index.js
@@ -1,26 +1,26 @@
 $(document).ready(function () {
-    const now = new Date();
-    $('#now').text(
-        now.getFullYear() + '-' + now.getMonth()+ '-' + now.getDate() + ' '  +
-        now.getHours() + ':' + now.getMinutes() + ':' +now.getSeconds() 
-    );
+    // const now = new Date();
+    // $('#now').text(
+    //     now.getFullYear() + '-' + now.getMonth()+ '-' + now.getDate() + ' '  +
+    //     now.getHours() + ':' + now.getMinutes() + ':' +now.getSeconds() 
+    // );
 
-    $('input[type="checkbox"]').bootstrapSwitch({
-        size: 'mini',
-        onText: '',
-        offText: '',
-        handleWidth: 10
-    });
+    // $('input[type="checkbox"]').bootstrapSwitch({
+    //     size: 'mini',
+    //     onText: '',
+    //     offText: '',
+    //     handleWidth: 10
+    // });
 
     $('#info-form').on('submit', e => {
         e.preventDefault();
         
         var result = $('#info-form').serializeArray().reduce(function(obj, item) {
-            if (item.name === 'ability') {
-                obj[item.name] = obj[item.name] || [];
-                obj[item.name].push(item.value);
-                return obj;
-            }
+            // if (item.name === 'ability') {
+            //     obj[item.name] = obj[item.name] || [];
+            //     obj[item.name].push(item.value);
+            //     return obj;
+            // }
             obj[item.name] = item.value;
             return obj;
         }, {});
